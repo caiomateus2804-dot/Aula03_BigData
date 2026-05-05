121f94cc0719445f3889a53e18f62849f256954b
Cdcaio.mateus <caio.mateus2804@gmail.com>
2026-05-04 21:48:09 -0300

    Altera o repositório

---
+++ aula03/Atividade01.py
@@ -0,0 +1,12 @@
+valor_compra = float(input("Informe o valor da compra"))
+
+if  valor_compra > 250:
+    desconto = valor_compra * 0.16
+    print(f'Perfeito! desconto realizado{desconto}')
+
+else:
+  print(f'Não! desconto não realizado {valor_compra}') 
+
+
+
+   

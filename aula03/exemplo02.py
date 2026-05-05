277c9cede8a92addb1cbc3abcb8ff5d4525374af
Cdcaio.mateus <caio.mateus2804@gmail.com>
2026-05-04 21:06:19 -0300

    Altera o repositório

---
+++ aula03/exemplo02.py
@@ -0,0 +1,24 @@
+idade = int(input('Digite a idade:'))
+
+# > maior, < menor, >=, <=, ==, !=
+
+if idade >= 18:
+    print('Você é adulto')
+else:
+    print('Você é menor de idade')
+
+# Classificação por pontos
+pontos = int(input('Informe os pontos:')) 
+
+if  pontos >= 100:
+    total >= pontos + 10
+    print(f'Excelente! Agora você têm {total}')
+ 
+elif pontos >= 50:
+     total = pontos + 5 
+     print(f'Bom desempenho. Você têm {total} pontos')
+
+else:
+     print(f'Treine mais. Pontuação {pontos} pontos')  
+
+     print('Fim!')
